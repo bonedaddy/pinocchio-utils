506c899b40d633b8c352410f289e6374ea03b8b4
bonedaddy <alex@bonedaddy.io>
2025-05-29 16:24:00 -0700

    add parse_u32

--- crates/putils/src/uint.rs
+++ crates/putils/src/uint.rs
@@ -5,6 +5,11 @@ pub fn parse_u64(data: &[u8]) -> u64 {
     u64::from_le_bytes(data.try_into().expect("slice must be 8 bytes"))
 }
 
+#[inline(always)]
+pub fn parse_u32(data: &[u8]) -> u32 {
+    u32::from_le_bytes(data.try_into().expect("slice must be 4 bytes"))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -19,8 +24,23 @@ mod test {
     }
 
     #[test]
+    fn test_parse_u32() {
+        let num = 69420_u32;
+        
+        let num2 = parse_u32(&num.to_le_bytes());
+
+        assert_eq!(num, num2);
+    }
+
+    #[test]
     #[should_panic(expected = "slice must be 8 bytes")]
     fn test_parse_u64_insufficient_length() {
         let _ = parse_u64(&[1, 2]);
+    }
+
+    #[test]
+    #[should_panic(expected = "slice must be 4 bytes")]
+    fn test_parse_u32_insufficient_length() {
+        let _ = parse_u32(&[1, 2]);
     }
 }
